b9301edb49d6cad2e13ba977f1fb3a1982671983
dikshanik <dikshabansod4010@gmail.com>
2022-06-07 14:55:03 +0000

    done with assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,15 @@
 const BookModel= require("../models/bookModel")
 
+//const BookModel= require("../models/bookModel")
+
 const createBook= async function (req, res) {
     let data= req.body
-
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
 const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
+    let allBooks= await BookModel.find()
     res.send({msg: allBooks})
 }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,13 +7,16 @@ const BookController= require("../controllers/bookController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
-
 router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
+
+//router.post("/createUser", UserController.createUser )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
