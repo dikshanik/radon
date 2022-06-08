991a6db8b4e65b35d76c4e13ab69004d97d10bbe
dikshanik <dikshabansod4010@gmail.com>
2022-06-08 18:42:07 -0400

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const route = require('./routes/route.js');
+// const { default: mongoose } = require('mongoose');
+// const app = express();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
@@ -8,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://diksha:diksha@cluster0.juhgc.mongodb.net/diksha-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/models/authorModel.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+const bookSchema = new mongoose.Schema( {
+
+    name : String,
+    authorId : Number,
+    price : Number,
+    ratings : Number
+
+
+    
+    
+}, { timestamps: true });
