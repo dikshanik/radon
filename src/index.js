23299723a340fcd292ec8a22b78fd439503788b6
dikshanik <dikshabansod4010@gmail.com>
2022-06-12 00:13:15 +0530

    finally done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+/*
+mongoose.connect("mongodb+srv://diksha:diksha@cluster0.juhgc.mongodb.net/diksha-db?retryWrites=true&w=majority",
+{useNewUrlParser: true}
+)
 
+.then(() => console.log('MongoDb is connected'))
+  .catch((err) => console.log(err));
+
+  */
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
